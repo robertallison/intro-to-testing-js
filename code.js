948f4e3f76bf3901d12d980ab90b89ef953750b4
Robert Allison <robert.r.allison4@gmail.com>
2021-08-25 09:41:10 -0500

    finished various exercises, will come back and work on this more

--- code.js
+++ code.js
@@ -3,6 +3,40 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-function sayHello() {
-    return "Hello";
+function sayHello(name) {
+    var name = "Jane"
+    return "Hello " + name;
+}
+
+
+function isFive() {
+    var number = prompt("Please input something");
+    return number == 5;
+}
+
+function isEven() {
+    var even = input % 2;
+    if (input == "boolean") {
+        return false;
+    } else {
+        return !!(even = 0);
+    }
+
+}
+
+function isVowel(input) {
+    input = input.toUpperCase();
+    if( input == "A") {
+        return true;
+    } else if (input == "E"){
+        return true;
+    } else if (input == "I"){
+        return true;
+    } else if (input == "O"){
+        return true;
+    } else if (input == "U"){
+        return true;
+    } else {
+        return false;
+    }
 }
--- tests.js
+++ tests.js
@@ -21,10 +21,41 @@ describe( "sayHello", function () {
     it("should return a string when called", function(){
         expect(typeof sayHello()).toBe("string");
     });
-    it("should return the string 'Hello' when executed", function(){
-        expect(sayHello()).toBe("Hello");
-    });
     it("should never return 'undefined' when called", function() {
         expect(sayHello()).not.toBe(undefined);
     });
-})
+    it("should return Hello, Jane", function() {
+        expect(sayHello(name)).toBe("Hello Jane");
+    });
+
+});
+
+describe("isFive", function(){
+    it("should be a defined function", function() {
+        expect(typeof isFive).toBe("function");
+    });
+    it("should return a boolean when called", function(){
+        expect(typeof isFive()).toBe("boolean");
+    });
+    it("should return true when input is 5", function(){
+        expect(isFive(5)).toBe(true);
+    });
+    it("should return true when input is the string '5'", function() {
+        expect(isFive('5')).toBe(true)
+    })
+});
+
+describe('isEven', function() {
+    it('should be a defined function', function() {
+        expect(typeof isEven).toBe('function');
+    });
+    it("should return a boolean when called", function(){
+        expect(typeof isEven()).toBe("boolean");
+    });
+    it("should return false when called with a boolean input", function(){
+        expect(isEven("false")).toBe(false);
+    });
+    it("should return false when called with a boolean input", function(){
+        expect(isEven("true")).toBe(false);
+    });
+});
